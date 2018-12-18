ca4f159ee1a0ef355003ecebfb30e7540633b0e4
lpjune <laurenporsini@gmail.com>
2018-12-18 11:09:56 -0600

    updated messages for server testing

--- pythonserver.py
+++ pythonserver.py
@@ -7,40 +7,35 @@ import serial
 def server_program():
     # get the hostname
     host = socket.gethostname()
-    port = 5555  # initiate port no above 1024
+    # initiate port no above 1024
+    port = 43434
     # get instance
     server_socket = socket.socket()
     # try to bind host address and port together
     try:
         server_socket.bind((host, port))
+        print("///// NEST Server Online /////")
     except socket.error as e:
         print(str(e))
     # configure how many client the server can listen simultaneously
     server_socket.listen(5)
-    conn, address = server_socket.accept()  # accept new connection
+    # accept new connection
+    conn, address = server_socket.accept()
     print("Connection from: " + str(address))
     while True:
-        listen()
-        conn.send(data.encode())  # send data to the client
+        print("Now listening...")
+        listening()
+        sending('hey you sent something')
+
     # close the connection
     conn.close()
 
-# all lan uses antenna right outside of nest
-def listen(from_device):
+def listening():
     data = conn.recv(1024).decode()
-    # app
-    if from_device == 'app':
-        app(data)
-    # arduino (motors) is wired to master computer
-    elif from_device == 'arduino':
-        arduino(data)
-    # pi (drone)
-    elif from_device == 'pi_drone':
-        pi_drone(data)
-    # latte panda (pressure matrix)
-    elif from_device == 'latte_panda':
-        latte_panda(data)
+    print(data)
 
+def sending(input):
+    server_socket.send(input.encode())
 # def send(to_device, data):
 
 
